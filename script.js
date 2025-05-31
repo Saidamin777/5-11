c5bac34dfc654f8e7ded8d8323f901fea0f098b2
Saidamin777 <saidakmalov1707@gmail.com>
2025-05-30 21:24:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let bgrang = prompt('Background Colorini kiriting')
+
+let rang = prompt('Text Colorini kiriting')
+
+let text = document.querySelector('#text')
+let ata = document.querySelector("#bgcolor")
+
+bgcolor.style.backgroundColor = `${bgrang}`
+
+text.style.color = `${rang}`
